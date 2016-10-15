b2b1a61728f7ffb7293268f2114bd2a1362e824a
Daffa Nugraha <daffa1699@gmail.com>
2016-10-15 19:43:02 +0700

    Inisialisasi

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl2009/formpendaftarankomunitas/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl2009/formpendaftarankomunitas/MainActivity.java
@@ -2,6 +2,9 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl2009.formpendaftarankomunitas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText etNama = (EditText) findViewById(R.id.editTextNama);
+        EditText etIden = (EditText) findViewById(R.id.editTextIdentitas);
+        EditText etTL   = (EditText) findViewById(R.id.editTextTL);
+        EditText etAlamat = (EditText) findViewById(R.id.editTextAlamat);
+        EditText etMail = (EditText) findViewById(R.id.editTextMail);
+        Spinner spKerja = (Spinner) findViewById(R.id.spinnerKerja);
+        RadioButton rbLaki = (RadioButton) findViewById(R.id.radioButtonLaki);
+        RadioButton rbPer = (RadioButton) findViewById(R.id.radioButtonPer);
+
     }
 }
